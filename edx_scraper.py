f8832fe48bfac0280d8a0703095efdc6205055f9
Serena Pei <serenapei@dhcp-10-29-244-179.dyn.MIT.EDU>
2024-01-25 18:29:49 -0500

    1/25 commit

--- edx_scraper.py
+++ edx_scraper.py
@@ -24,36 +24,46 @@ total_pages = 42
 #         curr_page.click()
         # get all course on current page
     
-driver.implicitly_wait(10)
+driver.implicitly_wait(20)
 courses = driver.find_elements(By.CLASS_NAME, "base-card-wrapper")
 # For each course, get dropdown to choose What you'll learn section
-first = courses[0]
-first.click()
-driver.implicitly_wait(10)
+driver.implicitly_wait(30)
 
-# for course in courses:
-# Get What you'll learn section
-driver.implicitly_wait(10)
-section = driver.find_elements(By.CLASS_NAME, "preview-expand-component")
-print(len(section))
-index = None
-# Find index of What you'll learn
-for test in range(len(section)):
-    print(section[test].find_element(By.TAG_NAME, "h1"))
-    if section[test].find_element(By.TAG_NAME, "h1").text == "What you'll learn":
-        index = test
-        break
+for course in courses:
+    driver.implicitly_wait(20)
+    course.click()
+    # Get course name
+    # title = driver.find_element(By.CLASS_NAME, "col-md-7")
+    # name = title.find_element(By.TAG_NAME, "h1").text
+    # print(name)
 
-# index into correct section
-# section = driver.find_elements(By.CLASS_NAME, "preview-expand-body")[index]
-# Iterate through bullet points
-driver.implicitly_wait(10)
-bullets = section[index].find_elements(By.TAG_NAME, "p")
+    # Get What you'll learn section
+    driver.implicitly_wait(30)
+    section = driver.find_elements(By.CLASS_NAME, "preview-expand-component")
 
-# extract text from the section
-driver.implicitly_wait(10)
-for bullet in bullets:
-    print("text", bullet.text)
+    index = None
+    # Find index of What you'll learn
+    for test in range(len(section)):
+        if section[test].find_element(By.TAG_NAME, "h1").text == "What you'll learn":
+            index = test
+            break
+
+    # index into correct section
+    # section = driver.find_elements(By.CLASS_NAME, "preview-expand-body")[index]
+    # Iterate through bullet points
+    driver.implicitly_wait(10)
+    bullets = section[index].find_elements(By.TAG_NAME, "p")
+    if not bullets:
+        bullets = section[index].find_elements(By.TAG_NAME, "li")
+
+    # extract text from the section
+    driver.implicitly_wait(10)
+    print(bullets)
+    for bullet in bullets:
+        print(bullet.text)
+
+    driver.implicitly_wait(10)
+    driver.back()
 
 
     # for course in courses:
